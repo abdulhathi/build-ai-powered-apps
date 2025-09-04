74df3c55fc5b0f41c3d85b6c4c556e2eb06babd5
Abdul Hathi Mohamed Hussain <abdulhathi@gmail.com>
2025-09-03 19:38:26 -0700

    Installing openai and setup the backend api/chat endpoint

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,16 @@
 import express from 'express'
 import type { Request, Response } from 'express'
 import dotenv from 'dotenv'
+import OpenAI from 'openai'
 
 dotenv.config()
 
+const openAIClient = new OpenAI({
+  apiKey: process.env.OPENAPI_API_KEY,
+})
+
 const app = express()
+app.use(express.json())
 
 const port = process.env.PORT || 3000
 
@@ -15,6 +21,19 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/api/hello', (req: Request, res: Response) => {
   res.json({ message: 'Hi Hello' })
+})
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+  const { prompt } = req.body
+
+  const response = await openAIClient.responses.create({
+    model: 'gpt-5-mini',
+    input: prompt,
+    temperature: 1,
+    max_output_tokens: 100,
+  })
+
+  res.json({ message: response.output_text })
 })
 
 app.listen(port, () => {
